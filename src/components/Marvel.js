88ff2e632ff311c4fb09e066ff861cb42bb89f29
amsuarezp <amsuarezp@gmail.com>
2020-11-26 17:46:01 -0500

    done targets

--- src/components/Marvel.js
+++ src/components/Marvel.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Marvel.css";
 
 export default function Marvel() {
-  const URL =
-    "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=458f93be4a6df21efee622fcd9637889&hash=03a1481a0b726e3d1c16008b571cae3d";
+  const URL = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=458f93be4a6df21efee622fcd9637889&hash=03a1481a0b726e3d1c16008b571cae3d";
   const [marvel, setMarvel] = useState([]);
 
   useEffect(() => {
-    console.log("hola hola");
+    console.log("hola");
     axios
       .get(URL)
       .then((response) => {
@@ -23,11 +23,24 @@ export default function Marvel() {
 
   return (
     <>
-      <div>
-        {marvel.map((ma) => (
-                <p>{ma.name}</p>
-        ))}
-      </div>
+      <section className="section-as__card">
+          <h1 className="text-align">Personajes de Marvel</h1>
+        <div className="container-as__card bd-grid-as__card">
+          {marvel.map((ma) => (
+           <div className="body-as__card">
+           <div className="image-card-container">
+             <img src={ma.thumbnail.path+"."+ma.thumbnail.extension} alt="j" className="img-as-card-three"/>
+           </div>
+           <h3 className="subtittle-as__card">{ma.name}</h3>
+           <div className="subtittle-image-as__card">
+             <p className="p-card-font text-align">
+             {ma.description}
+             </p>
+           </div>
+         </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
